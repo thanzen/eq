041cdf4eaf2231a843231e96f04b22cf6c09f81d
Han,Tao <taohan@tmwsystems.com>
2014-11-13 22:59:44 -0800

    added ginkgo

--- services/userservice/userservice_test.go
+++ services/userservice/userservice_test.go
@@ -2,8 +2,11 @@ package userservice_test
 
 import (
 	"testing"
+
+	"github.com/onsi/ginkgo"
 )
 
 func TestServices(t *testing.T) {
-
+	ginkgo.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Books Suite")
 }
