1e50fbccdde259ea9a53892a075dc8669a1582ac
Han,Tao <taohan@tmwsystems.com>
2014-11-14 19:39:56 -0800

    init the schema script and demo data

---
+++ config/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE user_meta
+(
+  id serial NOT NULL,
+  first_name text,
+  last_name text,
+  user_name text,
+  email text,
+  age integer,
+  country text,
+  city text,
+  post_code text,
+  cell_phone text,
+  home_phone text,
+  office_phone text,
+  fax text,
+  CONSTRAINT user_meta_pkey PRIMARY KEY (id)
+)
