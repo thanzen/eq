a4b913e49442778de2e4f81c701cb8e74a9e7769
thanzen <ht0331@gmail.com>
2015-02-22 13:31:36 -0800

    test

--- views/app.ts
+++ views/app.ts
@@ -1,11 +1,18 @@
-module Products {
-    export interface Scope {
-        greetingText: string;
+/// <reference path="../packages/React.TypeScript.DefinitelyTyped.0.1.12/react.d.ts" />
+
+//import React = require('react');
+
+
+interface Scope {
+    greetingText: string;
+}
+
+class Controller {
+    constructor($scope: Scope) {
+        $scope.greetingText = "Helloddd from TypeScrssddsipt + AngularJS";
     }
 
-    export class Controller {
-        constructor ($scope: Scope) {
-            $scope.greetingText = "Hello from TypeScript + AngularJS";
-        }
-    }
 }
+
+
+
