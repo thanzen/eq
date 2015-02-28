9d2a2bf9d80839d140c36d5167da255a5548c9d6
than <than@linkedtec.com>
2015-02-28 09:25:54 -0800

    init RoleApiController

--- controllers/api/user/role.go
+++ controllers/api/user/role.go
@@ -2,7 +2,6 @@ package user
 
 import (
     "github.com/thanzen/eq/controllers/base"
-//  "github.com/thanzen/eq/models/user"
     userServes "github.com/thanzen/eq/services/user"
     "github.com/thanzen/eq/models/user"
     "encoding/json"
@@ -11,6 +10,10 @@ import (
 type roleUserModel struct {
     roleId int64
     users []*user.User
+}
+type rolePermissionModel struct {
+    roleId int64
+    permissions []*user.Permission
 }
 
 type RoleApiController struct {
@@ -22,16 +25,34 @@ func NewRoleApiController() *RoleApiController {
     ct := &RoleApiController{roleService: userServes.RoleService{}}
     return ct
 }
+
 func (this *RoleApiController) GetRoles() {
     roles := this.roleService.GetAllRoles()
     this.Data["json"] = roles
     this.ServeJson()
 }
+
+//todo: permission key should be in a
 func (this *RoleApiController) AddUsers() {
+    if !this.UserService.HasPermission(this.User, "eq_role_adduserstorole") {
+        this.Ctx.Abort("401", "not authorized!")
+        return
+    }
     var model roleUserModel
-    json.Unmarshal(this.Ctx.Input.RequestBody, model)
-
-    this.roleService.InsertUsers(model.users)
-
+    json.Unmarshal(this.Ctx.Input.RequestBody, &model)
+    if model.roleId>0 && len(model.users)>0 {
+        this.roleService.InsertUsers(model.users)
+    }
 }
 
+func (this *RoleApiController) AddPermissions() {
+    if !this.UserService.HasPermission(this.User, "eq_role_addpermissionstorole") {
+        this.Ctx.Abort("401", "not authorized!")
+        return
+    }
+    var model rolePermissionModel
+    json.Unmarshal(this.Ctx.Input.RequestBody, &model)
+    if model.roleId>0 && len(model.permissions)>0 {
+        this.roleService.InsertUsers(model.permissions)
+    }
+}
